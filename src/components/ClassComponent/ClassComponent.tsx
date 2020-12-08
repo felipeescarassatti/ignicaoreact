63e89ac7e21fe7153ed61569a572391c02c7f190
Felipe Escarassatti <felipeescarassatti@gmail.com>
2020-12-07 22:26:21 -0300

    voltando a funciona

---
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class ClassComponent extends React.Component<{ name: string }> {
+    state = {
+        name: 'Mundo'
+    }
+
+    render() {
+        return <div>
+            <p>Name: { this.state.name }</p>
+            <button onClick={() => {
+                this.setState({ name: 'Felipe'})
+            }}>Click me</button>
+        </div>
+    }
+}
+
+export default ClassComponent
